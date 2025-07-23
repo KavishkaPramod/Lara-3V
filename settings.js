85aeef415847b1da97db08a8c198230017ca154c
KavishkaPramod <pramodkavishka133@gmail.com>
2025-07-23 09:50:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "KZRFWCoZ#Z_KE_5sZZeR45iYO_mroHe3WWJKm1Xqeo-kGIoQYmmI", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
+MODE: process.env.MODE || "private",  //public | private | inbox | group
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94741479321",
 DEV: process.env.DEV || "94779062397",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
